2c339d9b434606ed84cc7ad75f1656d9c50579ff
Freddie Tantoco <freddietantoco@Freddies-MacBook-Pro-2.local>
2022-06-30 12:17:47 -0700

    step3 basic form scaffolding

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,35 @@
 const Form = () => {
   return (
-    <div>
-      <h1>Login Form</h1>
-    </div>
+    <>
+    <h1 className="my-4">Login Form</h1>
+    <form>
+
+      <div className="form-group">
+        <label for="email">Email:</label>
+        <input 
+          className="form-control"
+          id="email" 
+          type="text" />
+        <span>Error message for email field.</span>
+      </div>
+
+      <div className="form-group">
+        <label for="password">Password:</label>
+        <input 
+          className="form-control"
+          id="password" 
+          type="password" />
+        <span>Error message for password field.</span>
+      </div>
+      
+      <div className="form-group mt-3">
+        <button 
+          disabled 
+          className="btn btn-primary btn-md"
+          type="submit">Log In</button>
+      </div>
+    </form>
+    </>
   );
 };
 
