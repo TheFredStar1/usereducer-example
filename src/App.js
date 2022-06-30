ff0e733244b2f62ff0f9550bf13c0dd19c84570e
Freddie Tantoco <freddietantoco@Freddies-MacBook-Pro-2.local>
2022-06-30 11:46:30 -0700

    step 1 - project start

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Form from './components/Form';
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="offset-md-4 col-md-4">
+          <Form/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
